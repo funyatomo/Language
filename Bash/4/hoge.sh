4de3a75a7762f9346e4b5ca9068fc291006f6cb1
funyatomo <tanaka.tomoaki.43m@st.kyoto-u.ac.jp>
2017-09-17 18:25:56 +0900

    modified

--- Bash/4/hoge.sh
+++ Bash/4/hoge.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # 配列の宣言
-# 先頭だけと全体の出力
+# 先頭だけと全体の出力 @,*
 array=(1 "hoge" 2 "aha" 3 "hogeaha")
 echo $array
 echo ${array[@]}
+echo ${array[*]}
 echo
 
+# インデックスには変数の利用も可能
 array=($(date))
-echo ${array[2]}
+index=2
+echo ${array[$index]}
 echo
 
 # 配列の要素を追加する
 echo ${array[@]}
 array+=("hoge" "aha" "123")
-echo ${array[@]}
-echo
+echo ${array[*]}; echo
 
+for i in "${array[@]}"; do echo $i; done
+echo
+for i in "${array[*]}"; do echo $i; done
